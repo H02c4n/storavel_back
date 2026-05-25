9abac8ebe0d2206dc4691430eb6ef2a57daa1cb5
Halil <svtr.hozcan@gmail.com>
2026-05-25 23:35:31 +0200

    db

--- config/settings/development.py
+++ config/settings/development.py
@@ -6,13 +6,22 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com',]
 
 INSTALLED_APPS += ['django_extensions']
 
-# SQLite kullan - PostgreSQL'i tamamen ez
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
+# # SQLite kullan - PostgreSQL'i tamamen ez
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 CORS_ALLOW_ALL_ORIGINS = True
 
