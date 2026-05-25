56df8a6746b2b53644876c83fe8abeb032e8b3f1
Halil <svtr.hozcan@gmail.com>
2026-05-25 23:42:16 +0200

    sonhal

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,8 +1,23 @@
 from .base import *
+import os
+import dj_database_url
 
 DEBUG = False
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '.onrender.com').split(',')
+
+# Database
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+
+CORS_ALLOWED_ORIGINS = [
+    os.getenv('FRONTEND_URL', 'http://localhost:3000'),
+]
