71b63c6f1c57b0944f255d5086448bb36562a17e
Halil <svtr.hozcan@gmail.com>
2026-05-26 09:46:21 +0200

    build son

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --run-syncdb
+python manage.py seed
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
