1303116250b9620995001a2ff71fee6f828b268a
Halil <svtr.hozcan@gmail.com>
2026-05-26 08:54:43 +0200

    build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --run-syncdb
 python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'back@storavel.se', 'deneme12345')
+if not User.objects.filter(email='back@storavel.se').exists():
+    User.objects.create_superuser(email='back@storavel.se', password='deneme12345')
     print('Superuser created')
 else:
     print('Superuser already exists')
